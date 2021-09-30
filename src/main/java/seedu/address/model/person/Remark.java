8f17bf22bb16a633fb20fc1c76beeef25fcafe9f
Mengzhe Wang <mengzhe_wang@u.nus.edu>
2021-10-01 01:17:38 +0800

    Fix style error

--- src/main/java/seedu/address/model/person/Remark.java
+++ src/main/java/seedu/address/model/person/Remark.java
@@ -9,6 +9,10 @@ import static java.util.Objects.requireNonNull;
 public class Remark {
     public final String value;
 
+    /**
+     * Constructor of Remark
+     * @param remark the remark message
+     */
     public Remark(String remark) {
         requireNonNull(remark);
         value = remark;
